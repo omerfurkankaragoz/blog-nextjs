41919cd6af8067c3ae1e89985449297ee98c05bf
omerfurkankaragoz <omerfurkankaragozz@gmail.com>
2021-04-27 00:59:16 +0300

    v.0.2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,47 @@
 function HomePage() {
-    return <div className="text-purple-400">Welcome to Next.js!</div>
+    return (
+        <>
+            <div className="site-container">
+                <div className="space-y-4">
+                    <h1 className="text-3xl font-bold">
+                        Merhaba ✋ Ben Ömer, Flutter ile mobil uygulamalar geliştiriyorum.
+                    </h1>
+
+
+                    <p>
+                        Eylül 2020 den itibaren Flutter öğrenme sürecine başladım ve her gün aktif öğrenme sürecimi geliştiriyorum.
+                        4 aylık bir süreçte fikir aşamasından ürün aşamasına çıkardığımız BiMaçVar! adındaki mobil uygulamamız marketlerde bulunuyor.
+                    </p>
+
+                    <p>
+                        BiMaçVar! projesinde Co-founder olarak yer almaktayım. Aynı zamanda tasarım, front-end kodlama ve satış-pazarlama kısımlarında aktif rol almaktayım.
+                        Şu an profesyonel bir ekiple Ternego adındaki sinema sektörü için bir mobil uygulama projesine Co-founder olarak yer almaktayım.
+                        full-stack developer ve satış-pazarlama kısımlarında aktif rol alıyorum.
+                    </p>
+
+                    <p>
+                        Bu projelerin pazarlama kısmında bir çok önemli bağlantı edindim.
+                        Yönetici seviyesinde bir çok insanla görüşme ve fikir alışverişinde bulunma şansı elde ettim.
+                        Bu projelerden öğrendiğim pazarlama, iletişim kurma ve ikna becerilerimi daha da geliştirmek için uğraşıyorum.
+                    </p>
+
+                    <p>
+                        Bunların yanı sıra JavaScript öğrenme sürecim şu an başlangıç aşamasında ve devam etmekte.
+                        Front-end ve Back-end kısmındaki bilgimi artırmak ve Flutter alanında kendimi geliştirmek istiyorum.
+                        Bunların yanı sıra insan ilişkilerindeki becerim, ikna kabiliyetim ve ekip uyumum ile çok yönlü bir kişilik kazanmaya
+                        ve bu becerilerimi günden güne artırmaya çalışıyorum.
+
+                    </p>
+                </div>
+            </div>
+            <div className="max-w-4xl mx-auto mt-20">
+                <img src="omer.JPG" alt="" />
+            </div>
+        </>
+
+
+    )
+
 }
 
 export default HomePage
